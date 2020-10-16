57abb0232c078580f4dbef68012419b74f84a2b6
abdsonya <sofya.abdurakhmanova@mail.ru>
2020-10-16 10:52:50 +0300

    Update test.cpp

--- sources/tests/test.cpp
+++ sources/tests/test.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
 #include "../Sort.h"
 
-TEST(Sorting, both)
+TEST(sorting, both)
 {
     const int dim = 7;
     int sequence1[dim] = {8, 3, 1, 0, -1, -3, -8};
     int sequence2[dim] = {-8, -3, -1, 0, 1, 3, 8};
-    sort::sort(sequence1, dim);
+    sort::Sort(sequence1, dim);
     for (int k = 0; k < dim; k++)
     {
         ASSERT_EQ(sequence[k], expected[k]);
@@ -18,7 +18,7 @@ TEST(sorting, odd)
     const int dim = 3;
     int sequence1[dim] = {7, 9, 5};
     int sequence2[dim] = {5, 7, 9};
-    Sort::Sort(sequence1, dim);
+    sort::Sort(sequence1, dim);
     for (int k = 0; k < dim; k++)
     {
         ASSERT_EQ(sequence[k], expected[k]);
@@ -29,7 +29,7 @@ TEST(Sorting, same)
 {
     const int dim = 4;
     int sequence1[dim] = {1, 1, 1, 1};
-    Sort::Sort(sequence1, dim);
+    sort::Sort(sequence1, dim);
     for (int k = 1; k < dim; k++)
     {
         ASSERT_EQ(sequence[k-1], expected[k]);
